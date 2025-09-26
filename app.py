a0ba50e2b08116718d779da2fa042834b28fd5b6
Khaled Ghalwash <115194209+khaled-dotcom@users.noreply.github.com>
2025-09-26 09:37:33 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+st.title("Khaled Chat Online 🤖")
+
+@st.cache_resource
+def load_model():
+    tokenizer = AutoTokenizer.from_pretrained("khaledghalwash/khaled_chatkkj")
+    model = AutoModelForCausalLM.from_pretrained("khaledghalwash/khaled_chatkkj")
+    return tokenizer, model
+
+tokenizer, model = load_model()
+
+def chat(user_input):
+    inputs = tokenizer(user_input, return_tensors="pt")
+    outputs = model.generate(**inputs, max_length=100)
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+user_input = st.text_input("اكتب كلامك هنا:")
+
+if st.button("أرسل"):
+    reply = chat(user_input)
+    st.write(reply)
